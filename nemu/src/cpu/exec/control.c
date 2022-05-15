20c6e84faddb872dfb5345d8f69509724184de3e
tracer-ics2017 <tracer@njuics.org>
2022-05-15 15:04:07 +0800

    >  compile 162020222 liuyicheng Linux debian 4.19.0-19-686 #1 SMP Debian 4.19.232-1 (2022-03-07) i686 GNU/Linux  15:04:07 up  4:45,  1 user,  load average: 0.00, 0.00, 0.00 76325d26c28f39c55d70cb416c659d7bf31180fc

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -45,7 +45,9 @@ make_EHelper(ret) {
 }
 
 make_EHelper(call_rm) {
-  TODO();
+  decoding.is_jmp = 1;
+  decoding.jmp_eip = id_dest->val;
+  rtl_push(&decoding.seq_eip);
 
   print_asm("call *%s", id_dest->str);
 }
